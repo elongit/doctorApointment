dab52b74b0e4f808ee7aa88b3b4a1c0e008b7049
ossama <ossama@gmail.com>
2024-11-21 16:05:02 -0600

    re style the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <header className="flex justify-between p-5 cursor-pointer bg-white ">
       <h1 className="font-bold text-3xl tracking-wider text-secondary-color">
-      <NavLink to="/">  Doct<span className="text-white bg-primary-color px-2 rounded">or</span></NavLink>
+      <NavLink to="/">  Doct<span className="text-white bg-primary-color px-2 rounded-md">or</span></NavLink>
       </h1>
       <nav className="">
         <ul className="flex gap-5 font-semibold text-[18px] text-black/75 items-center">
@@ -16,7 +16,11 @@ const Header = () => {
             <NavLink to="/contact">Need help?</NavLink>
          </li>
 
-          <li className="bg-primary-color text-white px-4 py-2 rounded">
+         <li>
+            <NavLink to="/doctorsList">Doctors?</NavLink>
+         </li>
+
+          <li className="bg-primary-color text-white px-4 py-2 rounded-md">
              <NavLink to="/login">Login</NavLink>
           </li>
         </ul>

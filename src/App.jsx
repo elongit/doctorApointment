cd0ae2d835afd6fd61179c544e8d4947afd728ed
ossama <ossama@gmail.com>
2024-11-21 16:04:08 -0600

    Added some routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Login from './pages/Login';
 import SignUp from "./pages/SignUp";
 import AuthHeader from "./components/authHeader";
 import Contact from "./pages/contact";
+import DoctorList from "./pages/doctorsList";
+import Home from "./pages/home";
 
 function App() {
   return (
@@ -29,9 +31,11 @@ function AppContent() {
       
       <main className="p-3 md:p-5 ">
         <Routes>
+          <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login />} />
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/doctorsList" element={<DoctorList/>}></Route>
         </Routes>
       </main>
       <Footer/>
